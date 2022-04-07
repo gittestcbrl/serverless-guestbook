f97ca27ee46657043de9bd8801daff8eb172fc8f
gittestcbrl <96676341+gittestcbrl@users.noreply.github.com>
2022-04-07 12:10:30 +0530

    Update guestbook.js
    URL changed

--- guestbook.js
+++ guestbook.js
@@ -1,7 +1,7 @@
 /**
  * Web application
  */
-const apiUrl = '';
+const apiUrl = 'https://79038871.eu-gb.apigw.appdomain.cloud/guestbook';
 const guestbook = {
   // retrieve the existing guestbook entries
   get() {
